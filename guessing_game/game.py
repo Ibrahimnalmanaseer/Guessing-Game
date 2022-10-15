d5f561a5b3514f5862ff600880ebdf79da9cd635
Ibrahim Almanaseer <ibrahimnalmanaseer@gmail.com>
2022-10-15 11:13:20 +0000

    done

--- guessing_game/game.py
+++ guessing_game/game.py
@@ -1,11 +1,12 @@
 from  random import randint
 
-class Game :
+class GuessingGame :
     result={}
     def __init__(self,player):
         self.player=player
         self.number=randint(1,20)
         self.score = 0
+        self.guessed_number=''
     def guess_number(self):
             count=5
             scores=[0,2,4,6,8,10]
@@ -13,24 +14,24 @@ class Game :
                 
 
                 try:
-                    guessed_number=int(input(f'Dear {self.player} enter your guess between (1-20)  '))
-                    if guessed_number == self.number:
+                    self.guessed_number=int(input(f'Dear {self.player} enter your guess between (1-20)  '))
+                    if self.guessed_number == self.number:
                         self.score=scores[count]
                         print(f'congrats ! , You win the number is {self.number} and your score is : {scores[count]} ')
                     
                         count = 0
-                    if guessed_number > self.number:
+                    if self.guessed_number > self.number:
                         count-=1
                         if count == 0:
                             print ('you lose ')
-                            Game.result[self.player]=scores[count]
+                            
                         else:
                             print(f'your guess too high ,{count} rounds left')
-                    if guessed_number < self.number:
+                    if self.guessed_number < self.number:
                         count-=1
                         if count == 0:
                             print ('you lose ')
-                            Game.result[self.player]=scores[count]
+                            
                         else:
                             print(f'your guess too low ,{count} rounds left')
                 except ValueError:
@@ -58,7 +59,10 @@ class Game :
 
 
 
-
+player =GuessingGame('ehab')
+print('game')
+player.guessed_number=10
+print(player.guess_number)
 
 
 
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,3 +1,43 @@
+from threading import active_count
+import pytest
+
+from guessing_game.game import GuessingGame
 
 
 
+def test_player_name():
+    player = GuessingGame('ibrahim')
+    actual = player.player
+    expected = "ibrahim"
+    assert actual == expected
+
+
+def test_random_num():
+    player=GuessingGame('ehab')
+    actual=player.number
+    excepted=actual > 0 and actual <21
+    assert excepted
+
+def test_game_score():
+
+    player=GuessingGame('ehab')
+    actual=player.score
+    excepted=0
+    assert excepted == actual
+
+
+def test_guessed_num():
+
+    player=GuessingGame('ehab')
+    actual=player.guessed_number
+    excepted=''
+    assert excepted == actual
+
+def test_input():
+    player =GuessingGame('hani')
+    player.guessed_number=10
+    player.number=10
+    actual=player.guess_number()
+    excepted =  print(f'congrats ! , You win the number is {player.number} and your score is : 10 ')  
+
+    assert excepted == actual
